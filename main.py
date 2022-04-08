a7a1ea532dd3fd98a5d609b2ac5545c90003b433
FedorovEg0r <fedorovegor239@gmail.com>
2022-04-08 20:48:19 +0300

    ex62

---
+++ main.py
@@ -0,0 +1,21 @@
+# import module
+
+
+# module.get_set()
+
+
+# module.get_txt()
+
+
+# print(module.list_doc.doc)
+
+
+# print(module.file_doc.doc)
+
+
+# print(module.get_binary())
+
+
+# print(dir(module))
+
+import figures
